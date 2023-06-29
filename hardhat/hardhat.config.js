da4785481d05930da33187515978dcfa05535538
Bram <Bram.mathijssen@gmail.com>
2023-06-29 21:21:47 +0200

    build: build data for DAO contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -6,6 +6,7 @@ require("hardhat-deploy")
 require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
@@ -26,6 +27,11 @@ module.exports = {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 5,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
